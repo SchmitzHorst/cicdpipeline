8c8779c6e7eb96961f7a6dc9ac82b87257ffbb6c
SchmitzHorst <horst_schmitz@gmx.de>
2025-12-04 18:56:22 +0100

    updated controller

--- src/main/java/com/hsc/cicdpipeline/web/TestController.java
+++ src/main/java/com/hsc/cicdpipeline/web/TestController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping(path = "/test")
+//@RequestMapping(path = "/test")
 public class TestController {
 
     @GetMapping
+    public ResponseEntity<Map<String, String>> healthcheck() {
+        return ResponseEntity.ok().body(Map.of("Status","Up"));
+    }
+
+    @GetMapping("/test")
     public ResponseEntity<Map<String, String>> test() {
-        return ResponseEntity.ok().body(Map.of("Testing","Up and Running"));
+        return ResponseEntity.ok().body(Map.of("Testing","Test Data"));
     }
 }
